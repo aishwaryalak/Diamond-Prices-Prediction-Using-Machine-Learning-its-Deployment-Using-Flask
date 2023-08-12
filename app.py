2f33f814a73c8008ce2d8309dfc19ff9a9b7d4de
aishwaryalak <83492300+aishwaryalak@users.noreply.github.com>
2023-08-12 16:22:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template,request
+import numpy as np
+import pickle
+
+flask_app=Flask(__name__)   
+model=pickle.load(open("model.pkl","rb"))
+
+@flask_app.route("/")        
+def Home():
+    return render_template("index.html")
+
+@flask_app.route("/predict",methods=['POST'])   
+def predict():
+    float_features=[float(x) for x in request.form.values()] 
+    features=[np.array(float_features)]  
+    prediction=model.predict(features)
+    return render_template("index.html",predicted_text="The Diamond Price is $ {}".format(prediction))
+
+if(__name__)=="__main__":
+    flask_app.run(debug=True)
